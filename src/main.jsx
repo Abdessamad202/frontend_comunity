c8cb1c79e01a2820883eb87d93f916495e03c807
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-04-10 02:30:43 +0100

    Update src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+// import "./css/index.css";
+import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { UserProvider } from './components/UserProvider.jsx';
+import { NotificationProvider } from './components/NotificationProvider.jsx';
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<NotificationProvider >
+				<QueryClientProvider client={queryClient}>
+					{/* <UserProvider> */}
+					<App />
+					{/* </UserProvider> */}
+				</QueryClientProvider>
+			</NotificationProvider>
+		</BrowserRouter>
+	</StrictMode>
 )
