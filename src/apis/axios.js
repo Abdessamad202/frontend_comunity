3b5c6708904ba4e50e204b131027c9a2e63e0036
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-04-10 02:30:44 +0100

    Add src/apis/axios.js

---
+++ src/apis/axios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://backend_comunity_app.test/api",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+});
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const user = JSON.parse(localStorage.getItem("user")) || {};
+        const token = user.token;
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default apiClient;
